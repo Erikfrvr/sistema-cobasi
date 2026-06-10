71ff24009c0948633c290c90baf07a52f5e79145
Erik Mauricio <erikmauricio3@hotmail.com>
2026-06-10 09:51:08 -0300

    Update tb_cliente tb_venda tb_pagamento.sql

--- Sistema Cobasi/tb_cliente tb_venda tb_pagamento.sql
+++ Sistema Cobasi/tb_cliente tb_venda tb_pagamento.sql
@@ -1,5 +1,5 @@
 --Estrutura de Clientes e Vendas: Scripts de tb_cliente, tb_venda, tb_pagamento (Responsável: Erik).
-
+CREATE DATABASE bd_cobasi;
 USE bd_cobasi;
 
  -- Tabela de clientes com saldo pro programa Amigo Cobasi
@@ -10,14 +10,24 @@ create table tb_cliente (
 	email varchar(100) not null unique,
 	telefone varchar(15),
 	senha varbinary(150) not null,
-	saldo_pontos INT DEFAULT 0 check (saldo_pontos >= 0)
+	saldo_pontos INT DEFAULT 0 check (saldo_pontos >= 0),
 	data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 
 );
 
 -- Tabela principal para registrar vendas
 create table tb_venda (
-	
+	id_venda INT PRIMARY KEY AUTO_INCREMENT,
+	id_cliente int not null,
+	data_venda DATETIME DEFAULT CURRENT_TIMESTAMP,
+	valor_total decimal(10,2) check (valor_total >= 0),
+	pontos_gerados int DEFAULT 0,
+	FOREIGN key (id_cliente) REFERENCES tb_cliente(id_cliente)
 
 
 );
+
+
+
+
+
